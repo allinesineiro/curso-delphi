3a5d40f1e079be931131b70afbe1f98f4fa6350b
Alline Sineiro Ribeiro <alline.qld.pack@alterdata.matriz>
2017-11-17 11:37:41 -0200

    Adicionando aula 3

---
+++ controle-15-minutos/Controle15Minutos.dpr
@@ -0,0 +1,13 @@
+program Controle15Minutos;
+
+uses
+  Forms,
+  UControle15Minutos in 'UControle15Minutos.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
